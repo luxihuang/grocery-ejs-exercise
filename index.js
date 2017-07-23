072ef1de1bbdd924b5056826836855e4de6d4283
luxihuang <Luxi.Huang@gmail.com>
2017-07-23 14:04:08 -0400

    grocery items

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 
 const express = require('express');
 const app = express();
-const parser = require('body-parser'); //npm install body-parser
+
+const parser = require('body-parser'); 
 app.use(parser.urlencoded({ extended: false }));
-app.use(express.static('public')) //css and images
 
 app.set('view engine', 'ejs'); // https://www.npmjs.com/package/ejs. everything is under the view folder
+app.use(express.static('public')) //css and images
 
-app.get('/list.ejs', (request, response) => {
-    response.render('index');
+
+const groceries =['bananas', 'milk', 'lettuce']
+
+app.get('/', (request, response) => {
+    response.render('index',groceries);
 });
 
-
-
-const server = app.listen(3000, () => {
+const server = app.listen(3010, () => {
     console.log('server started on port', server.address().port);
 });
